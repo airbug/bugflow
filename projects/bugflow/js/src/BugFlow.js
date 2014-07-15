22abf82a0e0a4938ed422c17fc7b9798c1bc22a6
Brian Neisler <neisler.brian@gmail.com>
2014-07-15 08:55:48 -0700

    lint fixes

--- projects/bugflow/js/src/BugFlow.js
+++ projects/bugflow/js/src/BugFlow.js
@@ -27,10 +27,10 @@ require('bugpack').context("*", function(bugpack) {
     // BugPack
     //-------------------------------------------------------------------------------
 
-    var Class               = bugpack.require('Class');
-    var Flows               = bugpack.require('Flows');
-    var Obj                 = bugpack.require('Obj');
-    var Proxy               = bugpack.require('Proxy');
+    var Class   = bugpack.require('Class');
+    var Flows   = bugpack.require('Flows');
+    var Obj     = bugpack.require('Obj');
+    var Proxy   = bugpack.require('Proxy');
 
 
     //-------------------------------------------------------------------------------
